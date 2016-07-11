29868a89b8ad1ddf04d202e34145ac562d392e41
Fábio Henrique <fabiosjc@gmail.com>
2016-07-10 22:47:04 -0300

    Seção 4, aula 23: Webpack: Custom Package Names
    Change-Id: Icd952e59c737de622a78dd447cf0d2b72b71fd2a

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
-var GreeterForm = require('./GreeterForm');
-var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('GreeterForm');
+var GreeterMessage = require('GreeterMessage');
 
 var Greeter = React.createClass({
   getDefaultProps: function () {
