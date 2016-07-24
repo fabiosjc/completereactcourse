10b68f9f73e2d137ce856506f26b8c5eed71b15c
Fábio Henrique <fabiosjc@gmail.com>
2016-07-24 19:21:56 -0300

    Seção 4, aula 25: Bonus
    Change-Id: I249138827870a4316360f9b86797e828ce868501

--- ReactBoilerplate/webpack.config.js
+++ ReactBoilerplate/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
-    alias: {      
+    alias: {
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -15,7 +15,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
