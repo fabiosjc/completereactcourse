74c8383b08bde16f952497e72d7fc55b6d4613d3
Fábio Henrique <fabiosjc@gmail.com>
2016-08-01 20:44:06 -0300

    Seção 5 - Aula 28: Create Our Pages
    Change-Id: I510119473b7e2e69543e2f4db47960944faa8400

---
+++ ReactWeather/app/components/About.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var About = React.createClass({
+
+  render: function() {
+    return (
+      <h3>About component</h3>
+    );
+  }
+
+});
+
+module.exports = About;
